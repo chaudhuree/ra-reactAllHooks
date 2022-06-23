8b363083ba94f2825e7d609561d14b8ccd4b1e59
chaudhuree <chaudhuree00@gmail.com>
2022-06-23 12:12:12 +0600

    ##useRef basics

--- src/UseRef.js
+++ src/UseRef.js
@@ -1,25 +1,17 @@
-import React,{useState,useEffect,useRef} from 'react'
+import React,{useRef} from 'react'
 
 function UseRef() {
-const [inputValue, setInputValue] = useState("");
-const count = useRef(0);
-const btnRef= useRef()
+    const inputElement = useRef();
 
-useEffect(() => {
-  count.current = count.current + 1;
-  btnRef.current.innerText=inputValue;
-});
-
+    const focusInput = () => {
+      inputElement.current.focus();
+    };
+  
 
 return (
     <>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <h1>Render Count: {count.current}</h1>
-      <button ref={btnRef} onClick={()=>setInputValue('')}>hi this is a button</button>
+       <input type="text" ref={inputElement} />
+      <button onClick={focusInput}>Focus Input</button>
     </>
   );
 }
