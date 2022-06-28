91fbc9c4a0e0d6d01f79538dfbc93fb351a0d04e
chaudhuree <chaudhuree00@gmail.com>
2022-06-29 01:55:30 +0600

    clean up

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import './App.css';
-import ContextApi from './FunctionContextComponent'
-import { ThemeProvider } from './ThemeContext';
 
-// function App() {
-//   return (
-//     <>
-//      <ContextApi/>
-//     </>
-//   );
-// }
 
 function App() {
   return (
-    <ThemeProvider>
-      <ContextApi />
-    </ThemeProvider>
+    <></>
   )}
 export default App;
